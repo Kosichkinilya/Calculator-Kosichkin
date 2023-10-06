b8e7b6caa6721fefb82c7c6ab43878d4806adc7b
visual <visual@LAB44-08>
2023-10-06 12:36:00 +0300

    только интерфейс

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,9 +9,34 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	//private void OnCounterClicked(object sender, EventArgs e)
-	//{
-		
-	//}
+    private void Btn_numbers(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Btn_plus(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btn_minus(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Btn_divide(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Btn_multiply(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Btn_reset(object sender, EventArgs e)
+    {
+
+    }
 }
 
