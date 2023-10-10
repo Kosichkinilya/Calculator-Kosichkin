f19aa4dc69f431c13239b5f75ab8fc2f5cad1f27
visual <visual@LAB17-13>
2023-10-10 08:55:26 +0300

    Попытки заставить работать калькулятор

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,44 +2,97 @@
 
 public partial class MainPage : ContentPage
 {
-	
+    private string currentInput = "";
+    private double firstNumber = 0;
+    private char operation = ' ';
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
     private void Btn_numbers(object sender, EventArgs e)
     {
-
+        Button button = sender as Button;
+        if (button != null)
+        {
+            int number = int.Parse(button.Text);
+            currentInput += number.ToString();
+            Entry_1.Text = currentInput;
+        }
     }
 
     private void Btn_plus(object sender, EventArgs e)
     {
-
+        PerformCalculation();
+        operation = '+';
     }
 
     private void btn_minus(object sender, EventArgs e)
     {
-
+        PerformCalculation();
+        operation = '-';
     }
 
     private void Btn_divide(object sender, EventArgs e)
     {
-
+        PerformCalculation();
+        operation = '/';
     }
 
     private void Btn_multiply(object sender, EventArgs e)
     {
-
+        PerformCalculation();
+        operation = '*';
     }
 
-    //private void Btn_reset(object sender, EventArgs e)
-    //{
-    //    currentInput = string.Empty;
-    //    reslt = 0,0;
-    //    CurrentOperator = String.Empty;
-    //    DisplayEntry.Text = "0";
-    //}
+    private void Btn_equals(object sender, EventArgs e)
+    {
+        PerformCalculation();
+        operation = ' ';
+    }
+
+    private void Btn_reset(object sender, EventArgs e)
+    {
+
+        currentInput = "";
+        firstNumber = 0;
+        operation = ' ';
+        Entry_1.Text = "0";
+    }
+
+
+    private void PerformCalculation()
+    {
+        if (!string.IsNullOrEmpty(currentInput))
+        {
+            double inputNumber = double.Parse(currentInput);
+            switch (operation)
+            {
+                case '+':
+                    firstNumber += inputNumber;
+                    break;
+                case '-':
+                    firstNumber -= inputNumber;
+                    break;
+                case '*':
+                    firstNumber *= inputNumber;
+                    break;
+                case '/':
+                    if (inputNumber != 0)
+                    {
+                        firstNumber /= inputNumber;
+                    }
+                    else
+                    {
+                        // вывести ошибку деления на ноль 
+                    }
+                    break;
+            }
+            currentInput = "";
+            Entry_1.Text = firstNumber.ToString();
+        }
+
+    }
 }
 
