7871a58eca3f3028f30c53037ee9b4b500ff66d7
visual <visual@LAB44-08>
2023-10-13 12:40:50 +0300

    Пока что пусто

---
+++ Xaml/MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿namespace Calculator_Kosichkin;
+
+public partial class MainPage : ContentPage
+{
+    private string currentInput = "";
+    private double firstNumber = 0;
+    private char operation = ' ';
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void Btn_numbers(object sender, EventArgs e)
+    {
+        Entry_1.Text += (sender as Button).Text; // нажатая кнопка выводится в entry
+
+    }
+
+    private void Btn_plus(object sender, EventArgs e)
+    {
+     // кнопка сложения
+    }
+
+    private void btn_minus(object sender, EventArgs e)
+    {
+       // кнопка вычитания 
+    }
+
+    private void Btn_divide(object sender, EventArgs e)
+    {
+       // кнопка деления
+    }
+
+    private void Btn_multiply(object sender, EventArgs e)
+    {
+       // кнопка умножения
+    }
+
+    private void Btn_equals(object sender, EventArgs e)
+    {
+        // кнопка равенства 
+    }
+
+    private void Btn_reset(object sender, EventArgs e)
+    {
+     // кнопка сброса 
+    }
+
+
+    private void PerformCalculation()
+    {
+        if (!string.IsNullOrEmpty(currentInput))
+        {
+            double inputNumber = double.Parse(currentInput);
+            switch (operation)
+            {
+                case '+':
+                    firstNumber += inputNumber;
+                    break;
+                case '-':
+                    firstNumber -= inputNumber;
+                    break;
+                case '*':
+                    firstNumber *= inputNumber;
+                    break;
+                case '/':
+                    if (inputNumber != 0)
+                    {
+                        firstNumber /= inputNumber;
+                    }
+                    else
+                    {
+                        // вывести ошибку деления на ноль 
+                    }
+                    break;
+            }
+            currentInput = "";
+            Entry_1.Text = firstNumber.ToString();
+        }
+
+    }
+}
+
---
+++ obj/Debug/net7.0-android/android/src/crc6458bc7f37e40c3f88/MainActivity.java
@@ -0,0 +1,48 @@
+package crc6458bc7f37e40c3f88;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("Calculator_Kosichkin.MainActivity, Calculator Kosichkin", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("Calculator_Kosichkin.MainActivity, Calculator Kosichkin", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("Calculator_Kosichkin.MainActivity, Calculator Kosichkin", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
